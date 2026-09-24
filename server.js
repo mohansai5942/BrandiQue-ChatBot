bf7ce86f32bab5a13f451544624b7c12284733ba
K. Mohan Rao <mohansai5942@gmail.com>
2026-09-24 16:44:45 +0530

    Allow chatbot API calls from website preview and production

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use((_req, res, next) => {
     "Content-Security-Policy",
     "frame-ancestors 'self' https://www.brandique.in https://brandique.in https://aistudio.google.com https://*.aistudio.google.com"
   );
-  res.setHeader("Access-Control-Allow-Origin", "https://www.brandique.in");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   if (_req.method === "OPTIONS") return res.sendStatus(204);
